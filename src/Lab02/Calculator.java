60b151fd6a7959ea86558765f543cb90ccd5c633
ark1987s <arkadiy1987@yandex.ru>
2023-03-10 14:07:56 +0300

    fixed name methods

--- src/Lab02/Calculator.java
+++ src/Lab02/Calculator.java
@@ -2,62 +2,62 @@ package Lab02;
 
 public class Calculator {
 
-    public int AddCalc(Integer x, Integer y) {
+    public int addCalc(Integer x, Integer y) {
 
         return x + y;
     }
 
-    public long AddCalc(Long x, Long y) {
+    public long addCalc(Long x, Long y) {
 
         return x + y;
     }
 
-    public double AddCalc(Double x, Double y) {
+    public double addCalc(Double x, Double y) {
 
         return x + y;
     }
 
-    public int SubCalc(Integer x, Integer y) {
+    public int subCalc(Integer x, Integer y) {
 
         return x - y;
     }
 
-    public long SubCalc(Long x, Long y) {
+    public long subCalc(Long x, Long y) {
 
         return x - y;
     }
 
-    public double SubCalc(Double x, Double y) {
+    public double subCalc(Double x, Double y) {
 
         return x - y;
     }
 
-    public int MultiCalc(Integer x, Integer y) {
+    public int multiCalc(Integer x, Integer y) {
 
         return x * y;
     }
 
-    public long MultiCalc(Long x, Long y) {
+    public long multiCalc(Long x, Long y) {
 
         return x * y;
     }
 
-    public double MultiCalc(Double x, Double y) {
+    public double multiCalc(Double x, Double y) {
 
         return x * y;
     }
 
-    public double DivCalc(Integer x, Integer y) {
+    public double divCalc(Integer x, Integer y) {
 
         return x*1.0 / y;
     }
 
-    public double DivCalc(Long x, Long y) {
+    public double divCalc(Long x, Long y) {
 
         return x*1.0 / y;
     }
 
-    public double DivCalc(Double x, Double y) {
+    public double divCalc(Double x, Double y) {
 
         return x / y;
     }
--- src/Lab02/Main.java
+++ src/Lab02/Main.java
@@ -11,24 +11,24 @@ public class Main {
         System.out.println("Results (addition, subtraction, multiplication, division):");
 
 
-        System.out.println(calculator.AddCalc(5, 88));
-        System.out.println(calculator.AddCalc(10000000L, 4000000L));
-        System.out.println(calculator.AddCalc(6.3, 5.0));
+        System.out.println(calculator.addCalc(5, 88));
+        System.out.println(calculator.addCalc(10000000L, 4000000L));
+        System.out.println(calculator.addCalc(6.3, 5.0));
         System.out.println('\n');
 
-        System.out.println(calculator.SubCalc(4, 7));
-        System.out.println(calculator.SubCalc(800L, 20L));
-        System.out.println(calculator.SubCalc(13.3, 7.4));
+        System.out.println(calculator.subCalc(4, 7));
+        System.out.println(calculator.subCalc(800L, 20L));
+        System.out.println(calculator.subCalc(13.3, 7.4));
         System.out.println('\n');
 
-        System.out.println(calculator.MultiCalc(2, 46));
-        System.out.println(calculator.MultiCalc(6L, 11L));
-        System.out.println(calculator.MultiCalc(5.1, 3.2));
+        System.out.println(calculator.multiCalc(2, 46));
+        System.out.println(calculator.multiCalc(6L, 11L));
+        System.out.println(calculator.multiCalc(5.1, 3.2));
         System.out.println('\n');
 
-        System.out.println(calculator.DivCalc(6, 4));
-        System.out.println(calculator.DivCalc(128L, 48L));
-        System.out.println(calculator.DivCalc(10.0, 8.0));
+        System.out.println(calculator.divCalc(6, 4));
+        System.out.println(calculator.divCalc(128L, 48L));
+        System.out.println(calculator.divCalc(10.0, 8.0));
         System.out.println('\n');
 
 
