b61eaef7b3e6a1e4c9f433417e379800fbc975f4
ark1987s <arkadiy1987@yandex.ru>
2023-03-10 12:09:33 +0300

    new

---
+++ src/Lab02/Calculator.java
@@ -0,0 +1,64 @@
+package Lab02;
+
+public class Calculator {
+
+    public int Addition(Integer x, Integer y) {
+
+        return x + y;
+    }
+
+    public long Addition(Long x, Long y) {
+        return x + y;
+    }
+
+    public double Addition(Double x, Double y) {
+        return x + y;
+    }
+
+    public int Subtraction(Integer x, Integer y) {
+
+        return x - y;
+    }
+
+    public long Subtraction(Long x, Long y) {
+
+        return x - y;
+    }
+
+    public double Subtraction(Double x, Double y) {
+
+        return x - y;
+    }
+
+    public int Multiplication(Integer x, Integer y) {
+
+        return x * y;
+    }
+
+    public long Multiplication(Long x, Long y) {
+
+        return x * y;
+    }
+
+    public double Multiplication(Double x, Double y) {
+
+        return x * y;
+    }
+
+    public int Division(Integer x, Integer y) {
+
+        return x / y;
+    }
+
+    public long Division(Long x, Long y) {
+
+        return x / y;
+    }
+
+    public double Division(Double x, Double y) {
+
+        return x / y;
+    }
+
+
+}
---
+++ src/Lab02/Sportsman.java
@@ -0,0 +1,81 @@
+package Lab02;
+
+public class Sportsman {
+
+    private String type;
+    private String name;
+    private Integer age;
+    private Integer height;
+    private Integer weight;
+    private String award;
+    private String country;
+
+    public Sportsman() {
+    }
+
+    public Sportsman(String type, String name, Integer age, Integer height, Integer weight, String award, String country) {
+        this.type = type;
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.award = award;
+        this.country = country;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String getAward() {
+        return award;
+    }
+
+    public void setAward(String award) {
+        this.award = award;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
