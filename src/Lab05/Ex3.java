ad80a13a45fa3d803e89aeaaa615a09ddbc65f73
ark1987s <arkadiy1987@yandex.ru>
2023-03-24 02:00:39 +0300

    lab05: update поместил слова в параметры метода

--- src/Lab05/Ex3.java
+++ src/Lab05/Ex3.java
@@ -2,15 +2,9 @@ package Lab05;
 
 public class Ex3 {
     public static void main(String[] args) {
-        String text = "С камушка на камушек порхал легкомысленный, бяка, воробышек а на прилежно оштукатуренной" +
-                " террасе искусно задрапированной, бяка, гобеленами с дефензивой кронштадтского батальона...";
-        String noCensorWord = "бяка";
-        String censoredText = "[вырезано цензурой]";
-        if (text.equals(changeWord(text, noCensorWord, censoredText))) {
-            System.out.println("Нецензурные слова отсутствуют");
-        } else {
-            System.out.println(changeWord(text, noCensorWord, censoredText));
-        }
+        changeWord("С камушка на камушек порхал легкомысленный, бяка, воробышек а на прилежно оштукатуренной" +
+                        " террасе искусно задрапированной, бяка, гобеленами с дефензивой кронштадтского батальона...",
+                "бяка", "[вырезано цензурой]");
     }
 
     public static String changeWord(String text, String noCensorWord, String censoredText) {
@@ -18,6 +12,7 @@ public class Ex3 {
         if (index >= 0) {
             text = text.replaceAll(noCensorWord, censoredText);
         }
+        System.out.println(text);
         return text;
     }
 }
